7730c62c90b2471942e41502f5a7b70a77433806
mae <mguil98@lsu.edu>
2024-10-15 16:43:24 -0500

    [ADDITION] Player movement script implemented working for 2 players

--- Exo-Genesis/Assets/Scripts/PlayerMovement.cs
+++ Exo-Genesis/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,92 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Variables")]
+    public float moveSpeed = 20f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private SpriteRenderer spr;
+
+
+    [Header("KeyCodes For Player1")]
+    public KeyCode leftKey = KeyCode.A;
+    public KeyCode rightKey = KeyCode.D;
+    public KeyCode upKey = KeyCode.W;
+    public KeyCode downKey = KeyCode.S;
+    public KeyCode interactionKey = KeyCode.LeftControl;
+    public KeyCode subInteractionKey = KeyCode.CapsLock;
+
+    [Header("KeyCodes For Player2")]
+    public KeyCode leftKey2 = KeyCode.LeftArrow;
+    public KeyCode rightKey2 = KeyCode.RightArrow;
+    public KeyCode upKey2 = KeyCode.UpArrow;
+    public KeyCode downKey2 = KeyCode.DownArrow;
+    public KeyCode interactionKey2 = KeyCode.RightControl;
+    public KeyCode subInteractionKey2 = KeyCode.Slash;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
+
+        ChangeControls();
+
+    }
+    private void ChangeControls()
+    {
+        if (gameObject.name == "Player2")
+        {
+            leftKey = leftKey2;
+            rightKey = rightKey2;
+            upKey = upKey2;
+            downKey = downKey2;
+            //not gonna be used for now
+            interactionKey = interactionKey2;
+            subInteractionKey = subInteractionKey2;
+        }
+    }
+
+    public void MovementInput()
+    {
+        moveInput = Vector2.zero;
+        if (Input.GetKey(leftKey))
+        {
+            moveInput.x = -1f;
+            spr.flipX = moveInput.x * -1 < 0;
+        }
+        else if (Input.GetKey(rightKey))
+        {
+            moveInput.x = 1f;
+            spr.flipX = moveInput.x * -1 < 0;
+        }
+        else
+        {
+            moveInput.x = 0f;
+        }
+        if (Input.GetKey(downKey))
+        {
+            moveInput.y = -1f;
+        }
+        else if (Input.GetKey(upKey))
+        {
+            moveInput.y = 1f;
+        }
+        else
+        {
+            moveInput.y = 0f;
+        }
+        rb.velocity = moveInput * moveSpeed;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
-        {
-            Debug.Log("test");
-        }
+
+        MovementInput();
+
     }
 }
