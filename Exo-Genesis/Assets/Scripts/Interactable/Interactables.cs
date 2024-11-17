b1a1fad46cd2547e6a999b55cad6944372e573fe
mae <mguil98@lsu.edu>
2024-11-16 18:03:26 -0600

    [MODIFICATION] added required component to interactables + healthpack

--- Exo-Genesis/Assets/Scripts/Interactable/Interactables.cs
+++ Exo-Genesis/Assets/Scripts/Interactable/Interactables.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Interactables : MonoBehaviour
 {
+
+    private void Awake()
+    {
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+        //to make it's set as a trigger
+        boxCollider.isTrigger = true;
+    }
+
     public string itemName; //name of the item
     public ItemType itemType; // type of item
 
